ea9357b67c182ec262591b3a550a40c9450db55d
pafmon <pafmon@gmail.com>
2020-09-17 15:47:19 +0200

    Perses GHA

--- setup/index.js
+++ setup/index.js
@@ -21,14 +21,38 @@ config.apk_path = rootPath+"/app/build/outputs/apk/app-debug.apk"
 
 configYAML = yaml.safeDump(config);
 
-// Write a string to another file and set the file mode to 0755
 try {
     fs.writeFileSync(rootPath+'/.perses-full.yml', configYAML);
   } catch(err) {
-    // An error occurred
     console.error(err);
   }
 
-console.log("MY_SECRET = <"+process.env.MY_SECRET+">");  
+console.log("Obtaining credentials.");  
+
+if( process.env.AWS_SECRET_KEY == undefined ){
+    console.error("AWS_SECRET_KEY not available in environment");
+    process.exit(1);  
+} else if( process.env.AWS_ACCESS_KEY == undefined ){
+    console.error("AWS_ACCESS_KEY not available in environment");
+    process.exit(1);  
+}else{
+
+    var credentials ={
+        access_key: process.env.AWS_ACCESS_KEY,
+        secret_key: process.env.AWS_SECRET_KEY,
+        key_path: "test",
+        key_name: "test"
+    } 
+    var credentialsYAML = yaml.safeDump(credentials);
+    
+    try {
+        fs.writeFileSync(rootPath+'/.perses-credentials.yml', credentialsYAML);
+      } catch(err) {
+        console.error(err);
+      }
+    
+}
+
+
 
 process.exit(0);
