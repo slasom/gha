0511a9e56943ee6debfc524e7047c185d38ec521
pafmon <pafmon@gmail.com>
2020-09-17 14:40:53 +0200

    Perses GHA

--- setup/index.js
+++ setup/index.js
@@ -3,6 +3,7 @@ console.log("Set up script init.");
 const yaml = require('js-yaml');
 const fs   = require('fs');
 const path   = require('path');
+const { config } = require('process');
 
  
 var rootPath=path.normalize(__dirname+'/../..');;
@@ -10,11 +11,24 @@ var configFileName = rootPath+"/.perses.yml";
 
 console.log("Loading Perses Config ("+configFileName+")...");
 try {
-  const doc = yaml.safeLoad(fs.readFileSync(configFileName, 'utf8'));
-  console.log(doc);
+  const config = yaml.safeLoad(fs.readFileSync(configFileName, 'utf8'));
+
 } catch (e) {
   console.error(e);
   process.exit(1);
 }
+
+config.apk_path = "Mipath!"
+
+configYAML = yaml.safeDump(config);
+
+// Write a string to another file and set the file mode to 0755
+try {
+    fs.writeFileSync(rootPath+'/.perses-full.yml', configYAML);
+  } catch(err) {
+    // An error occurred
+    console.error(err);
+  }
+
 
 process.exit(0);
